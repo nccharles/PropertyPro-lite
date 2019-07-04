a4ea8bad0c3086d06f4843e2ddd01f53fc738d4d
charles <nccharles1@gmail.com>
2019-07-04 04:38:37 +0200

    feature(signin): user can sign in

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,15 @@
 import Authentication from '../helpers/auth';
 import moment from 'moment';
+import proData from '../data/storage';
 class User {
     constructor() {
-        this.users = [];
+        this.users = proData.usersList;
     }
     signUp(data) {
         let userId=this.users.length + 1;
         const hashPassword = Authentication.hashPassword(data.password);
         
-        const tokenData = Authentication.generateToken(userId);
+        const tokenData = Authentication.generateToken([userId,data.password,data.email]);
       
         const newUser = {
             id: userId,
@@ -19,14 +20,15 @@ class User {
             password: hashPassword,
             phoneNumber: data.phoneNumber,
             address: data.address,
-            isAdmin: data.isAdmin,
+            isAdmin: false,
             created_on: moment.now(),
-            modified_on: moment.now(),
         };
         this.users.push(newUser);
         return newUser
     }
-
+    AllUsers() {
+        return this.users;
+      }
 
 }
 export default new User();
