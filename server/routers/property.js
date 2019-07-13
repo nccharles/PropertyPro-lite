a021cd57fd869470d386b937f876100da999bd61
charles <nccharles1@gmail.com>
2019-07-13 16:52:00 +0200

    bg(update): fixed to update property

--- server/routers/property.js
+++ server/routers/property.js
@@ -2,13 +2,13 @@ import { Router } from 'express';
 import Property from '../controllers/property';
 import { checkToken } from '../middleware/handleToken';
 import fileUpload from 'express-fileupload';
-import { validProperty } from '../helpers/validations';
+import { validProperty, validUpdate } from '../helpers/validations';
 const router = Router();
 router.use(fileUpload({
     useTempFiles: true,
   }));
 router.post('/',checkToken,validProperty, Property.addProperty);
-router.patch('/:propertyId', checkToken,validProperty, Property.updateProperty);
+router.patch('/:propertyId', checkToken,validUpdate, Property.updateProperty);
 router.patch('/:propertyId/sold', checkToken, Property.markSold);
 router.delete('/:propertyId', checkToken, Property.deleteProperty);
 router.get('/', Property.getAllProperty);
